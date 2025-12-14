818258440d77f5607b3b13e5d12c7ec03426437b
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2025-12-14 17:18:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/studymate_androiddevelopment/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.studymate_androiddevelopment
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.studymate_androiddevelopment.ui.theme.StudyMate_AndroidDevelopmentTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            StudyMate_AndroidDevelopmentTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    StudyMate_AndroidDevelopmentTheme {
+        Greeting("Android")
+    }
+}
