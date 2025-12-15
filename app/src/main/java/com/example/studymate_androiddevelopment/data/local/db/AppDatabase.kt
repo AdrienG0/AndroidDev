ab6a8fb3c3839360574a31de4e55d3df54c5142f
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2025-12-15 11:10:28 +0100

    dataBase class

---
+++ app/src/main/java/com/example/studymate_androiddevelopment/data/local/db/AppDatabase.kt
@@ -0,0 +1,18 @@
+package com.example.studymate_androiddevelopment.data.local.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.studymate_androiddevelopment.data.local.dao.CourseDao
+import com.example.studymate_androiddevelopment.data.local.dao.TaskDao
+import com.example.studymate_androiddevelopment.data.local.entity.CourseEntity
+import com.example.studymate_androiddevelopment.data.local.entity.TaskEntity
+
+@Database(
+    entities = [CourseEntity::class, TaskEntity::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun courseDao(): CourseDao
+    abstract fun taskDao(): TaskDao
+}
