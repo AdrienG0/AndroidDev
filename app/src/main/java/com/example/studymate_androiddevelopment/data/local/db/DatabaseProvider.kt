0bec367fc6c5679fb36a969c750744552061b2df
AdrienG0 <145970282+AdrienG0@users.noreply.github.com>
2025-12-15 11:11:01 +0100

    DataBase provider

---
+++ app/src/main/java/com/example/studymate_androiddevelopment/data/local/db/DatabaseProvider.kt
@@ -0,0 +1,22 @@
+package com.example.studymate_androiddevelopment.data.local.db
+
+import android.content.Context
+import androidx.room.Room
+
+object DatabaseProvider {
+
+    @Volatile
+    private var INSTANCE: AppDatabase? = null
+
+    fun getDatabase(context: Context): AppDatabase {
+        return INSTANCE ?: synchronized(this) {
+            val instance = Room.databaseBuilder(
+                context.applicationContext,
+                AppDatabase::class.java,
+                "studymate_db"
+            ).build()
+            INSTANCE = instance
+            instance
+        }
+    }
+}
